8626643e0c479f4831c2c3e59dda94448499e049
khashayar <kh.p1999@yahoo.com>
2019-04-10 13:02:33 +0430

    pages routes implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './pages/dashboard/dashboard.component'
+import { LoginComponent } from './pages/login/login.component'
+import { SignupComponent} from './pages/signup/signup.component'
+import {UserManagementComponent} from './pages/user-management/user-management.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '' , component : DashboardComponent },
+ { path: 'Login' , component :  LoginComponent},
+ { path: 'Signup' , component : SignupComponent},
+ { path: 'UserManagement' , component :  UserManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
