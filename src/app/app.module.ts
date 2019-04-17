e01d3a0c444e5712b2c9aefc308f36c22721b1bf
khashayar <kh.p1999@yahoo.com>
2019-04-17 19:17:23 +0430

    user management form designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -101,7 +101,8 @@ import { UserManagementComponent } from './pages/user-management/user-management
     MatTabsModule,
     MatTooltipModule,
     MatTreeModule,
-    MatBottomSheetModule
+    MatBottomSheetModule,
+    FormsModule
   ],
   providers: [
     { provide: CustomMatPaginatorIntl , useClass: CustomMatPaginatorIntl}
