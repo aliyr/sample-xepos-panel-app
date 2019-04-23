fac173be1a45afdd88f9d3f6385d91a6560f6044
khashayar-pakkhesal <kh.p1999@yahoo.com>
2019-04-23 18:59:26 +0430

    toast service implemented

---
+++ src/app/services/snackbar/snackbar.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+
+  constructor( private snackBar: MatSnackBar) { }
+  toastSuccess(message: string , buttonText: string , Duration: number) {
+  this.snackBar.open(message, buttonText, {
+    duration: Duration,
+    panelClass : ['success-snackbar']
+  });
+}
+toastAlert(message: string , buttonText: string , Duration: number) {
+  this.snackBar.open(message, buttonText, {
+    duration: Duration,
+    panelClass : ['alert-snackbar']
+  });
+}
+toastError(message: string , buttonText: string , Duration: number) {
+  this.snackBar.open(message, buttonText, {
+    duration: Duration,
+    panelClass : ['error-snackbar']
+  });
+}
+
+
+
+
+
+}
