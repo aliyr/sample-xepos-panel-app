f9fece8a8a8a251ae327b8f2a06c175b8bf4664c
Alious <aliyr7592@gmail.com>
2019-04-23 10:55:50 +0430

    error state matcher implemented

---
+++ src/app/validators/error-state-matcher.ts
@@ -0,0 +1,9 @@
+import {ErrorStateMatcher} from '@angular/material';
+import {FormControl, FormGroupDirective, NgForm} from '@angular/forms';
+
+export default class ErrorStateValidator implements ErrorStateMatcher {
+  public constructor(private errorCode: string) {}
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control.dirty && form.hasError(this.errorCode);
+  }
+}
