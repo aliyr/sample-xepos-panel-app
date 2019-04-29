a4798a1d24128735bbe7b9a105916c7c07ebbbf6
khashayar-pakkhesal <kh.p1999@yahoo.com>
2019-04-29 19:56:30 +0430

    menu default open condition + custom scrollbar + partial style implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import {
   trigger,
   state,
@@ -12,17 +12,35 @@ import {
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   animations: [
-    trigger('subNav', [
-      state('start', style({
-        width: '0px'
-      })),
-      state('end', style({
-        width: '300px'
-      })),
+    trigger("subNav", [
+      state(
+        "start",
+        style({
+          width: "0px"
+        })
+      ),
+      state(
+        "end",
+        style({
+          width: "300px"
+        })
+      )
       // transition('start => end', animate(500)),
       // transition('end => start', animate(500))
-    ]),
-  ],
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isMenuOpenDefault: boolean;
+
+  ngOnInit() {
+    this.checkWidthOnInit();
+  }
+
+  checkWidthOnInit() {
+    this.isMenuOpenDefault = true;
+    if (window.innerWidth < 1300) {
+      this.isMenuOpenDefault = false;
+    }
+  }
 }
