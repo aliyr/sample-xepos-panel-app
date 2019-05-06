1bc4d2bf1b10f2062f60ebf3d71c653f39efbabe
khashayar <kh.p1999@yahoo.com>
2019-05-06 15:28:31 +0430

    autocomplete input filter implemented + device form enhanced

--- src/app/pages/devices/devices-form/devices-form.component.ts
+++ src/app/pages/devices/devices-form/devices-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-devices-form",
@@ -6,7 +8,40 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./devices-form.component.scss"]
 })
 export class DevicesFormComponent implements OnInit {
-  constructor() {}
+  devicesEditForm: FormGroup;
+  locations: string[];
+  businessHints: string[];
+  constructor(private fb: FormBuilder, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.locations = ["location1", "location2", "location3", "location4"];
+    this.businessHints = [
+      "register1",
+      "register2",
+      "register3",
+      "register4",
+      "register5"
+    ];
+    this.devicesEditForm = this.fb.group({
+      name: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      business: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      location: ["", [Validators.required]]
+    });
+  }
+  cancelEditDeviceForm() {
+    this.router.navigate(["/devices"]);
+  }
+
+  saveDevicesEditForm() {
+    const confirmedData = {
+      name: this.devicesEditForm.get("name").value,
+      description: this.devicesEditForm.get("description").value,
+      business: this.devicesEditForm.get("business").value,
+      password: this.devicesEditForm.get("password").value,
+      location: this.devicesEditForm.get("location").value
+    };
+    console.log(confirmedData);
+  }
 }
