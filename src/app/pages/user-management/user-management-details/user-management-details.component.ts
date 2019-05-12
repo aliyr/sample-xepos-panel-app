3102817d61b2efb5c83e0010e4607c80318aa3fc
Alious <aliyr7592@gmail.com>
2019-05-12 17:45:04 +0430

    form event added for filter

--- src/app/pages/user-management/user-management-details/user-management-details.component.ts
+++ src/app/pages/user-management/user-management-details/user-management-details.component.ts
@@ -1,12 +1,18 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from "@angular/core";
 import { UserManagementService } from "app/services/user-management/user-management.service";
 import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 import { NgLog } from "app/decorators/nglog.decorator";
 import { Router } from "@angular/router";
-import {merge} from 'rxjs';
-import {startWith, switchMap} from 'rxjs/operators';
-import { map } from 'rxjs/operators';
-
+import { merge, Subject} from 'rxjs';
+import { startWith, switchMap } from "rxjs/operators";
+import { map } from "rxjs/operators";
+import { fromEvent } from 'rxjs';
 @Component({
   selector: "app-user-management-list",
   templateUrl: "./user-management-details.component.html",
@@ -17,64 +23,68 @@ export class UserManagementDetailsComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource([]);
   displayedColumns: string[];
   resultsLength = 0;
-  filterText = '';
+  filterSubject$ = new Subject<string>();
+  filterValue: string;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild('filterTable') filterTable: ElementRef;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('filterInput') filterInput: ElementRef;
   constructor(
     private userManagementService: UserManagementService,
     private router: Router
   ) {}
   ngOnInit() {
-    this.displayedColumns = [
-      "Name",
-      // "type",
-      // "userLocation",
-      "edit",
-      "delete"
-    ];
-    // this.updateMaterialTable();
+    this.displayedColumns = ["Name", "edit", "delete"];
   }
 
   ngAfterViewInit() {
-    debugger;
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    merge(this.sort.sortChange, this.paginator.page, this.filterText).pipe(
-      startWith({}),
-      switchMap(() => {
-        return this.userManagementService.getUsers('/odata/XBack/products/XBack.getProductsForGridView',this.paginator.pageIndex + 1 ,{name: this.sort.active , direction: this.sort.direction} ,this.paginator.pageSize );
-      }),
-      map(data => {
-        this.resultsLength = data['@odata.count'];
-        return data;
-      }),
-    ).subscribe(data => {
-      (this.dataSource.data as any) = data.value;
-      console.log(this.dataSource.data);
-    });
-  }
 
-  deleteUser(UserName): void {
-    this.userManagementService.ElementData.map((elem, index) => {
-      if (UserName === elem.displayName) {
-        this.userManagementService.ElementData.splice(index, 1);
-      }
-    });
-    this.dataSource = new MatTableDataSource(
-      this.userManagementService.ElementData
-    );
-    // this.updateMaterialTable();
+   const filterEvent = fromEvent(this.filterInput.nativeElement , 'keypress');
+
+    debugger;
+    // back to first page for sorting
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+   merge(this.sort.sortChange, this.paginator.page ,filterEvent)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          return this.userManagementService.getUsers(
+            "/odata/XBack/products/XBack.getProductsForGridView",
+            this.paginator.pageIndex + 1,
+            { name: this.sort.active, direction: this.sort.direction },
+            this.paginator.pageSize
+          );
+        }),
+        map(data => {
+          this.resultsLength = data["@odata.count"];
+          return data;
+        })
+      )
+      .subscribe(data => {
+        (this.dataSource.data as any) = data.value;
+        console.log(this.dataSource.data);
+      });
   }
+  //
+  // deleteUser(UserName): void {
+  //   this.userManagementService.ElementData.map((elem, index) => {
+  //     if (UserName === elem.displayName) {
+  //       this.userManagementService.ElementData.splice(index, 1);
+  //     }
+  //   });
+  //   this.dataSource = new MatTableDataSource(
+  //     this.userManagementService.ElementData
+  //   );
+  //   // this.updateMaterialTable();
+  // }
 
   updateUser(userID): void {
     this.router.navigate(["/user-management/form", userID]);
   }
 
   applyFilter(filterValue: string) {
-    // this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    return new Promise();
+    this.filterSubject$.asObservable();
   }
 
   // updateMaterialTable(): void {
--- src/app/services/user-management/user-management.service.ts
+++ src/app/services/user-management/user-management.service.ts
@@ -1,58 +1,11 @@
 import { Injectable } from "@angular/core";
-import { User } from "app/models/user";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: "root"
 })
 export class UserManagementService {
-  ElementData: User[] = [
-    {
-      displayName: "jon",
-      firstName: "jon",
-      surname: "joni",
-      emailAddress: "joni@gmail.com",
-      type: "admin",
-      userLocation: "near",
-      RFIDtoken: "1234",
-      timeAttendance: true,
-      wage: "0",
-      username: "jon jon",
-      backOfficePassword: "123456789",
-      fourDigitPassword: "1234",
-      frontOfficeFastLogin: true
-    },
-    {
-      displayName: "adam",
-      firstName: "adam",
-      surname: "adami",
-      emailAddress: "adam@gmail.com",
-      type: "accountant",
-      userLocation: "far",
-      RFIDtoken: "4321",
-      timeAttendance: false,
-      wage: "2",
-      username: "adam adam1",
-      backOfficePassword: "987654321",
-      fourDigitPassword: "2312",
-      frontOfficeFastLogin: true
-    },
-    {
-      displayName: "ben",
-      firstName: "ben",
-      surname: "benzade",
-      emailAddress: "ben@gmail.com",
-      type: "admin",
-      userLocation: "near",
-      RFIDtoken: "4316",
-      timeAttendance: true,
-      wage: "4",
-      username: "ben benzade",
-      backOfficePassword: "7467543",
-      fourDigitPassword: "3214",
-      frontOfficeFastLogin: false
-    }
-  ];
+
   constructor(private http: HttpClient) {}
 
   static URLGenerator(mainUrl, page , orderBy: {name , direction} , pageLength): string {
